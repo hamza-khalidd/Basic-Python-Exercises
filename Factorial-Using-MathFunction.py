a0569672beeae1095b4ea6d2df32a7f21675ba0c
Hamza Khalid <72040742+hamzaa-khalid@users.noreply.github.com>
2021-01-02 21:46:52 +0500

    Add files via upload

---
+++ Factorial-Using-MathFunction.py
@@ -0,0 +1,3 @@
+import math as m
+num = int(input('Enter Number: '))
+print('Factorial of', num, 'Is: ',m.factorial(num))
---
+++ Factorial.py
@@ -0,0 +1,7 @@
+fact = 1
+num = int(input('Enter Number: '))
+
+for i in range(1,num + 1):
+       fact = fact*i
+
+print("The factorial of",num,"is",fact)
